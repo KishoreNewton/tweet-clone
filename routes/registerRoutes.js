db6453a10553ca63873f877d58f6265d8b65f59e
Kishore Newton <kishorenke@gmail.com>
2020-11-21 01:08:08 +0530

    validate input

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -9,6 +9,21 @@ router.get('/register', (req, res, next) => {
   res.status(200).render('register');
 });
 
-router.post('/register', (req, res, next) => {});
+router.post('/register', (req, res, next) => {
+  const firstName = req.body.firstName.trim();
+  const lastName = req.body.lastName.trim();
+  const username = req.body.username.trim();
+  const email = req.body.email.trim();
+  const password = req.body.password;
+
+  const payload = req.body;
+
+  if(firstName && lastName && username && email && password) {
+
+  } else {
+      payload.errorMessage = "Make sure each field is a valid value"
+      res.status(422).send('register', payload)
+  }
+});
 
 module.exports = router;
