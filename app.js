d2f1d836741e831fe65eb988a035af000d5ec7f7
Kishore Newton <kishorenke@gmail.com>
2020-11-20 10:46:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  res.status(200).render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
